6984db9eff3cd691cdb4b09cecb79ff45b949742
Andrew Villa <andrew.villa9182@gmail.com>
2021-04-10 16:05:24 -0400

    reformatted main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import discord
 import os
 
-TOKEN = ""
-
 client = discord.Client()
+
+print(discord.__version__)
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    channel = message.channel
+    author = message.author
+    msg = message.content
+    print (channel.name)
+    print (channel.type)
+
+    if author == client.user:
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    if msg.startswith("$hello"):
+        await channel.send(content="Hello!")
 
-client.run(TOKEN)
-
-print("Hello World!\n")
+#client.run(os.getenv('TOKEN'))
+client.run('ODMwMjc3OTMwMzUyMzc3ODc2.YHEWlA.qyPvjAw8lWGsVBkmWgVSHakg_yo')
+#print("Hello World!\n")
